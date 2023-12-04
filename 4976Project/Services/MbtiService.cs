9ef0aff8f7ffa351859e5ec8dd4aa84d4e8f4798
Ellen Jung <90542859+ellenjung24@users.noreply.github.com>
2023-12-04 00:11:00 -0800

    Moved functions from blazor pages to the service page

--- 4976Project/Services/MbtiService.cs
+++ 4976Project/Services/MbtiService.cs
@@ -3,6 +3,8 @@ using System.Text.RegularExpressions;
 using _4976Project.Data;
 using _4976Project.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace _4976Project.Services
 {
@@ -41,6 +43,82 @@ namespace _4976Project.Services
         public async Task<List<ApplicationUser>> GetAllUsers()
         {
             return await _context.Users.ToListAsync();
+        }
+
+        public async Task<string> SendMessage(string combinedMessage)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://chatgpt-api7.p.rapidapi.com/ask"),
+                Headers = {
+                    { "X-RapidAPI-Key", "8b1b2714d2msh19c8e1bbadddbffp18f8a6jsn3862b5350c90" },
+                    { "X-RapidAPI-Host", "chatgpt-api7.p.rapidapi.com" },
+                },
+                Content = new StringContent("{\r \"query\": \"" + combinedMessage + "\"\r }")
+                {
+                    Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                var mbtiType = ExtractMBTITypes(json["response"].ToString()).FirstOrDefault();
+                return mbtiType;
+            }
+        }
+
+        public async Task<string> getDescription(string combinedMessage)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://chatgpt-api7.p.rapidapi.com/ask"),
+                Headers = {
+                { "X-RapidAPI-Key", "8b1b2714d2msh19c8e1bbadddbffp18f8a6jsn3862b5350c90" },
+                { "X-RapidAPI-Host", "chatgpt-api7.p.rapidapi.com" },
+                },
+                Content = new StringContent("{\r \"query\": \"" + combinedMessage + "\"\r }")
+                {
+                    Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                return json["response"].ToString();
+            }
+        }
+
+        public async Task<string> findMatch(string combinedMessage)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://chatgpt-api7.p.rapidapi.com/ask"),
+                Headers = {
+                { "X-RapidAPI-Key", "8b1b2714d2msh19c8e1bbadddbffp18f8a6jsn3862b5350c90" },
+                { "X-RapidAPI-Host", "chatgpt-api7.p.rapidapi.com" },
+                },
+                Content = new StringContent("{\r \"query\": \"" + combinedMessage + "\"\r }")
+                {
+                    Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                return json["response"].ToString();
+            }
         }
     }
 }
